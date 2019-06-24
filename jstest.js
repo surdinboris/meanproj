4caaceec4319ebe677e10d3878f54457d886b12e
borrik <borrik@live.ru>
2019-06-24 19:30:58 +0300

    first commit

--- jstest.js
+++ jstest.js
@@ -1,2 +1,16 @@
-const mn= require("mongoose");
-console.log(mn);
+const mongoose= require("mongoose");
+var userSchema = mongoose.Schema({
+    firstName: String,
+    lastName: String
+});
+
+var Author = mongoose.model('Author', userSchema);
+var jamieAuthor = new Author({firstName:'Paul', lastName:'Lawson'});
+
+console.log(jamieAuthor);
+
+
+mongoose.connect('mongodb://localhost/mongoose_basics').then(res=>{
+    jamieAuthor.save(function(err) {
+        if (err) throw err;})
+});
